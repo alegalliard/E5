854d5e39009878a8b3af5f97010d8eba07c8e65f
alessandra.souza <alessandra.souza@metasix.com.br>
2017-08-31 22:21:16 -0300

    block2 - media query

--- inc/home/block2.php
+++ inc/home/block2.php
@@ -1,24 +1,42 @@
-<div class="container">
-    <section class="row">
-       <article class="col s5 text-center">
+<section class="row">
+    <article class="container">
+       
+       <figure class="col s12 m6 l7 right m-r-0">
+            <img src="img/ilustra-principal.svg" alt="Organograma ilustrativo">
+        </figure>
+        
+       <div class="col m6 l4 text-center left">
            <h1 class="teal-title">um portal para os eventos do DCTA</h1>
            <p class="e6-desc">Aqui você encontrará informações importantes sobre o Centro. Poderá acompanhar as atividades de formação contínua sobre a prática pedagógica no ensino superior, organizadas pelas Comissões Locais e pelo ITA. Qualquer dúvida, entre em contato pelo e-mail <a href="mailto:contato.e5@ita.br">contato.e5@ita.br</a> ou telefone (12) 3456-7890.</p>
            
-           <hr class="snake">
-           
+           <div class="hide-on-med-and-down">
+               <hr class="separador">
+
+               <p class="e6-bold">Solicite uma conta e tenha acesso a eventos e projetos</p>
+
+               <button type="button" class="btn-line">
+                   <span class="ic-sun"><i class="ic-login"></i></span>
+                   login ou cadastrar
+                </button>
+
+               <a href="#" class="mini-a">saiba mais</a>
+           </div>
+       </div>
+       
+       <div class="col s8 offset-s2 m6 offset-m3 text-center hide-on-large-only">
+           <hr class="separador">
+
            <p class="e6-bold">Solicite uma conta e tenha acesso a eventos e projetos</p>
-           
+
            <button type="button" class="btn-line">
                <span class="ic-sun"><i class="ic-login"></i></span>
                login ou cadastrar
             </button>
-           
+
            <a href="#" class="mini-a">saiba mais</a>
-       </article>
-       <figure class="col s9 offset-s2 m-r-0">
-          <img
-          src="img/ilustra-principal.svg"
-          alt="Organograma ilustrativo">
-        </figure>
-    </section>
-</div>
+       </div>
+       
+       
+       
+    </article>
+</section>
