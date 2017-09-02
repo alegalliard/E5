f76a860d410c818d2d1cdb12d19bbee1a633f39e
alessandra.souza <alessandra.souza@metasix.com.br>
2017-09-02 10:48:44 -0300

    block 4 - projetos em alta responsivo

--- inc/home/block3.php
+++ inc/home/block3.php
@@ -5,67 +5,75 @@
     <div class="news">
     <ul>
         <li class="news-box">
-           <figure class="news-pic"><img src="" alt=""></figure> 
-           <h3 class="h3-title">Prêmio desafia estudantes a solucionar case de comunicação</h3>
-           <p>Tema é "O custo da água e seu real valor para toda a sociedade".</p>
-           
-           <p class="info-box">
-               <i class="ic-user"></i>
-               <span class="author caption">Alex Hill</span>
-               <time class="date subcaption">22 Jun - leitura: 9 min aprox.</time>
-           </p>
+           <figure class="news-pic"><img src="" alt=""></figure>
+           <div class="info-block">
+               <h3 class="h3-title">Prêmio desafia estudantes a solucionar case de comunicação</h3>
+               <p class="news-desc">Tema é "O custo da água e seu real valor para toda a sociedade".</p>
+
+               <p class="info-box">
+                   <i class="ic-user"></i>
+                   <span class="author caption">Alex Hill</span>
+                   <time class="date subcaption">22 Jun - leitura: 9 min aprox.</time>
+               </p>
+           </div>
+        </li>
+        
+        <li class="news-box">
+           <figure class="news-pic"><img src="" alt=""></figure>
+           <div class="info-block">
+               <h3 class="h3-title">Programa ITA Talks discute a política de drogas</h3>
+               <p class="news-desc">Reitor recebe secretário-geral da Corte Permanente de Arbitragem</p>
+
+               <p class="info-box">
+                   <i class="ic-user ic-tiny"></i>
+                   <span class="author caption">Bryan Jackson</span>
+                   <time class="date subcaption">22 Jun - leitura: 7 min aprox.</time>
+               </p>
+            </div>
+        </li>
+        
+        
+        <li class="news-box">
+           <figure class="news-pic"><img src="" alt=""></figure>
+           <div class="info-block">
+               <h3 class="h3-title">ITA promove Feira das Profissões entre os dias 24 e 26/08</h3>
+               <p class="news-desc">EXTENSÃO</p>
+
+               <p class="info-box">
+                   <i class="ic-user ic-tiny"></i>
+                   <span class="author caption">Kevin Brocks</span>
+                   <time class="date subcaption">10 Jun - leitura: 7 min aprox.</time>
+               </p>
+            </div>
         </li>
         
         <li class="news-box">
            <figure class="news-pic"><img src="" alt=""></figure> 
-           <h3 class="h3-title">Programa ITA Talks discute a política de drogas</h3>
-           <p>Reitor recebe secretário-geral da Corte Permanente de Arbitragem</p>
-           
-           <p class="info-box">
-               <i class="ic-user ic-tiny"></i>
-               <span class="author caption">Bryan Jackson</span>
-               <time class="date subcaption">22 Jun - leitura: 7 min aprox.</time>
-           </p>
-        </li>
-        
-        
-        <li class="news-box">
-           <figure class="news-pic"><img src="" alt=""></figure> 
-           <h3 class="h3-title">ITA promove Feira das Profissões entre os dias 24 e 26/08</h3>
-           <p>EXTENSÃO</p>
-           
-           <p class="info-box">
-               <i class="ic-user ic-tiny"></i>
-               <span class="author caption"></span>
-               <time class="date subcaption">10 Jun - leitura: 7 min aprox.</time>
-           </p>
+           <div class="info-block">
+               <h3 class="h3-title">Cursos de graduação em todas as áreas do conhecimento</h3>
+               <p class="news-desc">Programa ITA Talks discute a política de drogas.</p>
+
+               <p class="info-box">
+                   <i class="ic-user ic-tiny"></i>
+                   <span class="author caption">Howard Santiago</span>
+                   <time class="date subcaption">02 Jun - leitura: 15 min aprox.</time>
+               </p>
+            </div>
         </li>
         
         <li class="news-box">
            <figure class="news-pic"><img src="" alt=""></figure> 
-           <h3 class="h3-title">Cursos de graduação em todas as áreas do conhecimento</h3>
-           <p>Programa ITA Talks discute a política de drogas.</p>
-           
-           <p class="info-box">
-               <i class="ic-user ic-tiny"></i>
-               <span class="author caption">Howard Santiago</span>
-               <time class="date subcaption">02 Jun - leitura: 15 min aprox.</time>
-           </p>
-        </li>
-        
-        <li class="news-box">
-           <figure class="news-pic"><img src="" alt=""></figure> 
-           <h3 class="h3-title">Cursos de graduação em todas as áreas do conhecimento</h3>
-           <p>Programa ITA Talks discute a política de drogas.</p>
-           
-           <p class="info-box">
-               <i class="ic-user ic-tiny"></i>
-               <span class="author caption">Howard Santiago</span>
-               <time class="date subcaption">02 Jun - leitura: 15 min aprox.</time>
-           </p>
-        </li>
-            
-        
+           <div class="info-block">
+               <h3 class="h3-title">Cursos de graduação em todas as áreas do conhecimento</h3>
+               <p class="news-desc">Programa ITA Talks discute a política de drogas.</p>
+
+               <p class="info-box">
+                   <i class="ic-user ic-tiny"></i>
+                   <span class="author caption">Howard Santiago</span>
+                   <time class="date subcaption">02 Jun - leitura: 15 min aprox.</time>
+               </p>
+            </div>
+        </li>        
     </ul>
   </div>
        
--- inc/home/block4.php
+++ inc/home/block4.php
@@ -1,19 +1,22 @@
 <section class="row">
     <div class="container">
-        <h3 class="yellow-title">Projetos em alta</h3>
-        <small><a href="">+ ver todos</a></small>
+        <h3 class="teal-subtitle">projetos em alta</h3>
+        <small class=""><a href="">+ ver todos</a></small>
         
         <hr>
         <?php
         for($i=0; $i < 5; $i++){ ?>
             
-        <article class="row">
-            <figure class="col s3">
-                <img src="" alt="">
-            </figure>
+        <article class="row big-news">
+            <div class="col s12 m4 l3">
+                <figure class="news-pic">
+                    <img src="" alt="">
+                </figure>
+            </div>
             
-            <div class="col s7">
+            <div class="col s12 m8 l5">
                 <h3 class="">Bibliometria: uma ferramenta estatística para a gestao da informação e do conhecimento, em sistemas de informação e de avaliação científica</h3>
+                
                 <p>Este artigo tem como objetivo o desenvolvimento de uma revisão da literatura sobre Bibliometrie, edstacando sua aplicação como ferramenta estatística básica para gestão</p>
                 <footer class="info-box side-icon">
                    <i class="ic-login"></i>
@@ -24,18 +27,19 @@
                </footer>
             </div>      
             
-            <aside class="col s5">
+            <aside class="col s12 m12 l4">
                <h4 class="caption">palavras-chave</h4>
                 <ul class="tags">
-                    <li class="tag"><a href="">Biometria</a></li>
-                    <li class="tag"><a href="">Gestão da Informação</a></li>
-                    <li class="tag"><a href="">Festão do Conhecimento</a></li>
-                    <li class="tag"><a href="">Sistemas de Informação</a></li>
-                    <li class="tag"><a href="">Comunicação</a></li>
-                    <li class="tag"><a href="">Gestão Tecnológica</a></li>
+                    <li class="tag"><a href="#">Biometria</a></li>
+                    <li class="tag"><a href="#">Gestão da Informação</a></li>
+                    <li class="tag"><a href="#">Festão do Conhecimento</a></li>
+                    <li class="tag"><a href="#">Sistemas de Informação</a></li>
+                    <li class="tag"><a href="#">Comunicação</a></li>
+                    <li class="tag"><a href="#">Gestão Tecnológica</a></li>
                 </ul>
             </aside>     
         </article>
+        <hr class="half-separator" />
         
        <?php } ?>
         
