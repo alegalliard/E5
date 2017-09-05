01827ed20feadf3e5cf262a93078329344a72743
alessandra.souza <alessandra.souza@metasix.com.br>
2017-09-04 22:41:51 -0300

    calendar - jqueryUI

--- index.php
+++ index.php
@@ -9,8 +9,24 @@ $date = date('h-i-s-a', time());
     <title>Document</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"> 
     <link rel="stylesheet" href="css/main.css?<?php echo $date ; ?>">
+    <link rel="stylesheet" href="vendor/jquery-ui-1.12.1.custom/jquery-ui.min.css">
+    <link rel="stylesheet" href="vendor/jquery-ui-1.12.1.custom/jquery-ui.theme.min.css">
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/materialize/js/bin/materialize.min.js"></script>
+    <script src="vendor/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+    <script>
+        $( function() {
+            $( "#datepicker" ).datepicker({
+                autoSize: true,
+                showOtherMonths: true,
+                selectOtherMonths: true,
+                dateFormat: "dd-mm-yy",
+                monthNames: [ "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" ],
+                dayNamesMin:["D", "S", "T", "Q", "Q", "S", "S"]
+
+            });
+        });
+    </script>
 </head>
 <body>
    <?php
